6982ba5c101f315504a92c48aa111ea46963552f
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2020-10-06 13:50:41 +0200

    liaison d'interpolation de variable {{ }}.

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./exercice1.component.css"],
 })
 export class Exercice1Component {
-  title = "Mon exemple";
-  number = "42";
-  bool = "true";
-  Objt = {
-    cle: "valeur",
-  };
+  public Carrer(nbr: number): number {
+    return nbr * nbr;
+  }
 }
