3ff15d3137718ab15e3619cad9604d7affc27afe
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2020-10-06 12:46:00 +0200

    liaison d'évènement avec ()

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -5,9 +5,12 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./exercice1.component.html",
   styleUrls: ["./exercice1.component.css"],
 })
-export class Exercice1Component implements OnInit {
+export class Exercice1Component {
   public type_attribut = "date";
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor() {
+    setTimeout(() => {
+      this.type_attribut = "text";
+    }, 3000);
+  }
 }
